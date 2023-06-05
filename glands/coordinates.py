980d90bc81792a9240b89af4d8514cf62b8767d8
astrofle <psalas@nrao.edu>
2023-06-05 13:03:44 -0400

    Added documentation.

--- glands/coordinates.py
+++ glands/coordinates.py
@@ -6,8 +6,16 @@ import astropy.units as u
 from astropy.coordinates import EarthLocation, AltAz, FK5
 
 
-def transform_to_radec(table):
+def transform_to_radec(table, equinox="J2000"):
     """
+    Transforms the coordinates in a table from AzEl to Equatorial.
+
+    Parameters
+    ----------
+    table : recarray
+        SDFITS table with CRTYPE2/3 in Az El.
+    equinox : str
+        Equinox for the equatorial coordinates.
     """
 
     gbt = EarthLocation.of_site("GBT")
@@ -15,7 +23,7 @@ def transform_to_radec(table):
     azel = AltAz(table["CRVAL2"]*u.deg, table["CRVAL3"]*u.deg, 
                  obstime=table["DATE-OBS"], location=gbt)
 
-    radec = azel.transform_to(FK5(equinox="J2000"))
+    radec = azel.transform_to(FK5(equinox=equinox))
 
     table = update_table_coo(table, radec)
 
@@ -24,6 +32,14 @@ def transform_to_radec(table):
 
 def update_table_coo(table, newcoo):
     """
+    Changes the coordinate axes in the SDFITS `table` to the values defined by `newcoo`.
+
+    Parameters
+    ----------
+    table : recarray
+        SDFITS table with CRTYPE2/3 in Az El.
+    newcoo : array
+        New coordinates. It assumes they are RA and Dec.
     """
 
     table["CTYPE2"] = "RA"
